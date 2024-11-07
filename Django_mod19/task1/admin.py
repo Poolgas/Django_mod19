1cb0ade13d9f2dd8be9be293cafc3164e3f1fa26
Poolgas <Stealik174@gmail.com>
2024-11-08 04:44:30 +0500

    Домашнее задание по теме "Сайт администрирования

--- Django_mod19/task1/admin.py
+++ Django_mod19/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Buyer, Game
+
 
 # Register your models here.
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name', 'age')
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'description', 'age_limited')
+    list_filter = ('title', 'cost', 'age_limited')
+    search_fields = ('title', 'cost', 'age_limited')
+    list_per_page = 5
+
+
--- Django_mod19/task1/models.py
+++ Django_mod19/task1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    age = models.PositiveIntegerField(max_length=3)
+    age = models.CharField(max_length=3)
 
     def __str__(self):
         return self.name
